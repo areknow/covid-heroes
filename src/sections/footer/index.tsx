6e01f3a885eb1a46c044144e0049592735715b31
Arnaud Crowther <arnaud.crowther@dynatrace.com>
2020-12-20 18:03:17 -0500

    added sass vars for media queries

--- src/sections/footer/index.tsx
+++ src/sections/footer/index.tsx
@@ -4,20 +4,15 @@ import site from '../../data/site.json';
 export const Footer = () => {
   return (
     <div className={styles.footer}>
-      <div className={styles.pageColumn + ' page-column'}>
+      <div className={'page-column ' + styles.pageColumn}>
         <h4>{site.title}</h4>
         <div className={styles.linkGroups}>
           <div>
             <h4>{site.content.footer.learn.header}</h4>
             <ul>
               {site.content.footer.learn.links.map((link, index) => (
-                <li>
-                  <a
-                    key={index}
-                    href={link.link}
-                    target="_blank"
-                    rel="noreferrer"
-                  >
+                <li key={index}>
+                  <a href={link.link} target="_blank" rel="noreferrer">
                     {link.label}
                   </a>
                 </li>
@@ -28,13 +23,8 @@ export const Footer = () => {
             <h4>{site.content.footer.about.header}</h4>
             <ul>
               {site.content.footer.about.links.map((link, index) => (
-                <li>
-                  <a
-                    key={index}
-                    href={link.link}
-                    target="_blank"
-                    rel="noreferrer"
-                  >
+                <li key={index}>
+                  <a href={link.link} target="_blank" rel="noreferrer">
                     {link.label}
                   </a>
                 </li>
