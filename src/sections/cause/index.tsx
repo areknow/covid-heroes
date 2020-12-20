e78e4f86483f0d9ed91d3d8a438e5babf894803d
Arnaud Crowther <arnaud.crowther@dynatrace.com>
2020-12-20 17:08:25 -0500

    migrated cause

--- src/sections/cause/index.tsx
+++ src/sections/cause/index.tsx
@@ -4,14 +4,11 @@ import styles from "./index.module.scss";
 
 export const Cause = () => {
   return (
-    <div
-      id="cause"
-      className={styles.section + " " + styles.cause + " " + "page-column"}
-    >
+    <div id="cause" className={"section " + styles.cause + " page-column "}>
       <h2 className="header">{site.content.sections.cause.header}</h2>
-      {/* {{#each site.content.sections.cause.paragraphs }
-    <p>{{ this }}</p>
-  {{/each}} */}
+      {site.content.sections.cause.paragraphs.map((paragraph, index) => (
+        <p key={index}>{paragraph}</p>
+      ))}
     </div>
   );
 };
