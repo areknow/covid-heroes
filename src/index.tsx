46cc95965c362bd5a5d53f650adb6466e1686dcc
Arnaud Crowther <arnaud.crowther@dynatrace.com>
2020-12-20 17:46:59 -0500

    migrated footer

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import Hero from './sections/hero';
 import Cause from './sections/cause';
 import Need from './sections/need';
 import Contact from './sections/contact';
+import Footer from './sections/footer';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -12,6 +13,7 @@ ReactDOM.render(
     <Cause />
     <Need />
     <Contact />
+    <Footer />
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/sections/footer/index.tsx
@@ -0,0 +1,40 @@
+import styles from './index.module.scss';
+import site from '../../data/site.json';
+
+export const Footer = () => {
+  return (
+    <div className={styles.footer}>
+      <div className={styles.pageColumn + ' page-column'}>
+        <h4>{site.title}</h4>
+        <div className={styles.linkGroups}>
+          <div>
+            <h4>{site.content.footer.learn.header}</h4>
+            <ul>
+              {site.content.footer.learn.links.map((link, index) => (
+                <li>
+                  <a key={index} href={link.link} target="_blank">
+                    {link.label}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div>
+            <h4>{site.content.footer.about.header}</h4>
+            <ul>
+              {site.content.footer.about.links.map((link, index) => (
+                <li>
+                  <a key={index} href={link.link} target="_blank">
+                    {link.label}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
