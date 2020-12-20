6b4482c35f10dc0bcbdb03facbe9b8dec4548ac3
Arnaud Crowther <arnaud.crowther@dynatrace.com>
2020-12-20 16:34:30 -0500

    migration from static site to react

---
+++ src/components/nav/Nav.tsx
@@ -0,0 +1,17 @@
+import styles from "./Nav.module.scss";
+import "../../styles/index.scss";
+import site from "../../site.json";
+
+export const Nav = () => {
+  return (
+    <nav>
+      <div className="page-column">
+        <ul className={styles.menu}>
+          <li>{site.title}</li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Nav;
