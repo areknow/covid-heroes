363d8d720ca9cd0b13fb313d93c5249d238fc177
Arnaud Crowther <arnaud.crowther@dynatrace.com>
2020-12-22 15:14:52 -0500

    created page-column component

---
+++ src/components/page-column/index.tsx
@@ -0,0 +1,7 @@
+import styles from './index.module.scss';
+
+const PageColumn = ({ children }: { children: React.ReactNode }) => {
+  return <div className={styles.pageColumn}>{children}</div>;
+};
+
+export default PageColumn;
