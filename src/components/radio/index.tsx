81116a1fbb23a51c23ed4655d50ef76042d0e809
Arnaud Crowther <arnaud.crowther@dynatrace.com>
2020-12-22 15:14:59 -0500

    created radio component

---
+++ src/components/radio/index.tsx
@@ -0,0 +1,19 @@
+import styles from './index.module.scss';
+
+interface RadioProps {
+  label: string;
+  group: string;
+  change: () => void;
+}
+
+const Radio = (props: RadioProps) => {
+  return (
+    <label className={styles.radioContainer}>
+      <input type="radio" name={props.group} onChange={() => props.change()} />
+      <span className={styles.radio}></span>
+      <div className={styles.label}>{props.label}</div>
+    </label>
+  );
+};
+
+export default Radio;
