cbb404640dcca6f897452fdb3146124df9076165
Arnaud Crowther <arnaud.crowther@dynatrace.com>
2020-12-24 14:35:31 -0500

    router map

--- src/app.tsx
+++ src/app.tsx
@@ -3,19 +3,21 @@ import Home from './pages/home';
 import Privacy from './pages/privacy';
 import Terms from './pages/terms';
 
+const routes = [
+  { path: '/privacy', component: Privacy },
+  { path: '/terms', component: Terms },
+  { path: '/', component: Home }
+];
+
 const App = () => {
   return (
     <Router>
       <Switch>
-        <Route path="/privacy">
-          <Privacy />
-        </Route>
-        <Route path="/terms">
-          <Terms />
-        </Route>
-        <Route path="/">
-          <Home />
-        </Route>
+        {routes.map((route, index) => (
+          <Route key={index} path={route.path}>
+            {route.component}
+          </Route>
+        ))}
       </Switch>
     </Router>
   );
